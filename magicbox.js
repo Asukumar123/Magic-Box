f9d0aee13e4e3fcab18808ccaa22da78e2e5eef3
asu kumar <asukumar.ug21.cs@nitp.ac.in>
2022-09-11 17:27:22 +0530

    first commit

---
+++ magicbox.js
@@ -0,0 +1,18 @@
+
+
+
+// function (i){
+    
+//         var score =0;
+    
+    
+
+var score=0;
+var answer=i=>{
+   
+  
+    var value= 2**i;
+    score=score+value;
+
+    document.getElementById('answer').innerHTML=score;
+}
